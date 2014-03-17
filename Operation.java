bf3be583e63f8d7e0e7a6331380a05cb6b6031a3
GraffT <sanekpanov@gmail.com>
2014-03-17 09:43:32 +0300

    Update Operation.java

--- Operation.java
+++ Operation.java
@@ -2,6 +2,7 @@
  * Created by Admin on 14.03.14.
  */
 public  interface Operation {
+     // AP: абстракт не нужно добавлять в интерфейсе
      abstract MatrixObject calculate(MatrixObject m1, MatrixObject m2);
      abstract Vector vCalculate(Vector v1, Vector v2);
 }
